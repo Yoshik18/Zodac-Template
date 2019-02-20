2b8ad9cc0ea1412f461e2266e7956f8b6879e915
Yoshik <yoshikkarnawat@gmail.com>
2019-02-20 17:32:20 +0530

    Done Till Sponsors

--- js/app.js
+++ js/app.js
@@ -20,13 +20,36 @@ window.addEventListener('scroll', function(){
     const nav = document.querySelector('.navbar');
     if(window.scrollY > 20){
         console.log('Logged');
-        nav.setAttribute('style', 'background: linear-gradient(to right, #8e2de2, #5D26C1)');
+        nav.setAttribute('style', 'background: linear-gradient(to right, #8e2de2, #5D26C1);  box-shadow: 1px 1px 5px black;');
     }else{
-        nav.setAttribute('style', 'background-color:transparent !important');
+        nav.setAttribute('style', 'background-color:transparent !important;');
     }
 })
 
 
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2,
+            autoplay: false
+        },
+        900:{
+            items: 2
+        },
+        1000:{
+            items:3
+        }
+    }
+})
 
 
 
